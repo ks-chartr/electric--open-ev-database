e31fa012b09688e5bb54e4fc312589e9d7a10146
atul <atul_jain@bitbucket.org>
2018-10-29 23:48:49 +0530

    minors

---
+++ Opendata/static/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    // $('.download_btn').on('click', function (e) {
+    //
+    //
+    //     let self = this;
+    //     let url = $(self).parent().attr('href');
+    //     console.log(url);
+    //
+    //     $.fileDownload(url, {
+    //         preparingMessageHtml: "We are preparing your report, please wait...",
+    //     failMessageHtml: "There was a problem generating your report, please try again."
+    //     });
+    //
+    //     return false;
+    // });
+});
