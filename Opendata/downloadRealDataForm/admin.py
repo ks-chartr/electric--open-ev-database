6686ed75b79f1d8907dbe6fe0e9b944395cfd112
Varun Bansal <varun.bansal@myntra.com>
2018-11-20 01:10:42 +0530

    show secret key in modal

--- Opendata/downloadRealDataForm/admin.py
+++ Opendata/downloadRealDataForm/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from .models import *
 # Register your models here.
-admin.site.register(DownloadRealData)
-# class DownloadRealDataAdmin(admin.ModelAdmin):
-    # exclude = ('passCode',)
+# admin.site.register(DownloadRealData)
+class DownloadRealDataAdmin(admin.ModelAdmin):
+    exclude = ('passCode',)
 
-# admin.site.register(DownloadRealData, DownloadRealDataAdmin)
+admin.site.register(DownloadRealData, DownloadRealDataAdmin)
