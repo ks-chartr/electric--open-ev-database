453bd59531c3201f71dda857090e4b1aade2e3f0
kshitij <kshitij18099@iiitd.ac.in>
2022-07-15 20:10:27 +0530

    upgraded to postgresql

--- Opendata/Opendata/settings.py
+++ Opendata/Opendata/settings.py
@@ -16,7 +16,6 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -78,17 +77,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Opendata.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':
+        {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': config('DB_NAME', default=None, cast=str),
+         'USER': config('DB_USER', default=None, cast=str),
+         'PASSWORD': config('DB_PASSWORD', default=None, cast=str),
+         'HOST': config('DB_HOST', default=None, cast=str),
+         'PORT': config('DB_PORT', default=None, cast=int)}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -108,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -121,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
